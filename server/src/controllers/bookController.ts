b44ddd14ad9bd10af83612f0cdee7bea7029832e
abuHafsGit <muhammadabuhafz@gmail.com>
2026-03-10 07:02:03 +0600

    Implement update and delete book functionalities in bookController and update routes

--- server/src/controllers/bookController.ts
+++ server/src/controllers/bookController.ts
@@ -38,3 +38,50 @@ export const addBook = async (req: Request, res: Response) => {
         })
     }
 }
+
+
+export const updateBook = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+    const { name, author, publishYear, description } = req.body;
+
+    try {
+        const book = await Book.findByIdAndUpdate(id, { name, author, publishYear, description })
+
+        if (!book) {
+            return res.status(404).json({
+                success: false,
+                message: "Book not found"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Book updated ",
+            book: book
+        })
+    } catch (error) {
+
+    }
+}
+
+
+export const deletBook = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+
+        const book = await Book.findByIdAndDelete(id)
+
+        if (!book) {
+            return res.status(404).json({
+                success: false,
+                message: "Book not found"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "book deleted"
+        })
+    } catch (error) {
+
+    }
+}
--- server/src/routes/bookRouts.ts
+++ server/src/routes/bookRouts.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { addBook, getBook } from "../controllers/bookController"
+import { addBook, deletBook, getBook, updateBook } from "../controllers/bookController"
 
 const bookRouter = Router()
 
 bookRouter.get('/get-book', getBook)
 bookRouter.post('/add-book', addBook)
+bookRouter.put('/update-book/:id', updateBook)
+bookRouter.delete('/delete-book/:id', deletBook)
 
 export default bookRouter
